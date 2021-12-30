9599eff681e24338e4e732c64bbea4689fbd6efe
Kex <kexanone@gmail.com>
2021-12-30 08:52:27 +0100

    Update factory building list

--- components/task/script_component.hpp
+++ components/task/script_component.hpp
@@ -4,7 +4,7 @@
 #define TLD_END_DELAY 72
 
 #define TLD_STAGE_ONE_TASKS ["TLD_task_destroyRadar", "TLD_task_destroyFactory", "TLD_task_destroyXians"]
-#define TLD_FACTORY_PARTS [TLD_FACTORY_PART_1, TLD_FACTORY_PART_2, TLD_FACTORY_PART_3, TLD_FACTORY_PART_4, TLD_FACTORY_PART_5, nearestBuilding [6213, 16277, 0]]
+#define TLD_FACTORY_PARTS [TLD_FACTORY_PART_1, TLD_FACTORY_PART_2, TLD_FACTORY_PART_3, TLD_FACTORY_PART_4, TLD_FACTORY_PART_5, TLD_FACTORY_PART_6, nearestBuilding [6213, 16277, 0]]
 #define TLD_XIANS [TLD_XIAN_1, TLD_XIAN_2, TLD_XIAN_3, TLD_XIAN_4]
 
 #define TLD_STAGE_TWO_TASKS ["TLD_task_killCollabs", "TLD_task_destroyComms", "TLD_task_destroyVarsuks"]
